933bce79b5eb0b48693de359af36c0468585ca2b
yeonjaegit <keo0713@naver.com>
2025-12-31 14:28:01 +0900

    vite.config.js복구

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'http://192.168.1.78:5000',
+        changeOrigin: true,
+      },
+      '/dashboard': {
+        target: 'http://192.168.1.78:5000',
+        changeOrigin: true,
+      },
+      '/sensor': {
+        target: 'http://192.168.1.78:5000',
+        changeOrigin: true,
+      }
+    }
+  }
+})
