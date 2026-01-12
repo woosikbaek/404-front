b017edecdd9658acfbaf8d05a6fd4611ac632b0f
suhwan-99 <kaghop9@gmail.com>
2026-01-12 11:53:58 +0900

    콘솔창에 뜨는 웹소켓 디버그 코드 삭제

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -19,12 +19,14 @@ export const socket = io(API_BASE, {
 export const getStompClient = () => {
     const sock = new SockJS(API_CHAT);
     const stompClient = Stomp.over(sock);
+    stompClient.debug = () => {};
     return stompClient;
 };
 // 3. 스케줄 관리 클라이언트 생성 함수 (8080번 서버용)
 export const getSchedulerClient = () => {
     const sock = new SockJS(API_SCHEDULER);
     const stompClient = Stomp.over(sock);
+    stompClient.debug = () => {};
     return stompClient;
 };
 // 기본적으로는 Socket.io를 내보냄
